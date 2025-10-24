9912cbc9277be36ad6a3c86ab58c2c0ad5b935c4
Djoubarry <mdjouldebarry17@gmail.com>
2025-10-24 06:25:31 +0000

    Fonction pour la creation des carre

--- script.js
+++ script.js
@@ -4,11 +4,30 @@ const title = document.createElement("h1")
 title.textContent = "==== Etch-A-Sketch ===="
 body.appendChild(title)
 
-const btnNew = document.createElement('button')
-btnNew.textContent = "Nouvelle grille"
-btnNew.classList.add("btnNew")
-body.appendChild(btnNew)
+const btnNewGrille = document.createElement('button')
+btnNewGrille.textContent = "Nouvelle grille"
+btnNewGrille.classList.add("btnNewGrille")
+body.appendChild(btnNewGrille)
 
 const container = document.createElement('div')
 container.classList.add("container")
 body.appendChild(container);
+
+let size = 16;
+
+function createGrid () {
+    container.innerHTML = "";
+
+    const squareSize = 960 / size;
+
+    for (i = 0; i < size * size; i++) {
+        const square = document.createElement('div')
+        square.classList.add("square")
+
+        square.style
+    }
+}
+
+
+createGrid(size)
+
